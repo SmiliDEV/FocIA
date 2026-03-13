11594d457fb54393c8d8a5c431124e80df0852cc
Afonso Henriques <nt.sm7li@gmail.com>
2026-03-13 12:07:50 +0000

    structure

---
+++ built/main.js
@@ -0,0 +1,6 @@
+"use strict";
+const app = document.getElementById("app");
+if (app) {
+    app.innerHTML = "<h1>Hello TypeScript Web</h1><p>Project is wired correctly.</p>";
+}
+//# sourceMappingURL=main.js.map
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+const app = document.getElementById("app");
+
+if (app) {
+  app.innerHTML = "<h1>Hello TypeScript Web</h1><p>Project is wired correctly.</p>";
+}
